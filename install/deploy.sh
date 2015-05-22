f9f574719360e7ef4cc50824c9304645c39dec27
Zahari Zahariev <zahari.ven.zahariev@hp.com>
2015-05-22 06:44:00 +0100

    Redo MySQL noninteractive installation

--- install/deploy.sh
+++ install/deploy.sh
@@ -1,12 +1,13 @@
-sudo apt-get remove lighttpd php5-cgi php5-mysql
-sudo apt-get autoremove
+sudo apt-get -y remove mysql-server mysql-server-5.5 lighttpd php5-cgi php5-mysql
+sudo apt-get -y autoremove
 sudo apt-get autoclean
-sudo apt-get install lighttpd php5-cgi php5-mysql debconf-utils
+sudo apt-get -y install lighttpd php5-cgi php5-mysql
 sudo lighttpd-enable-mod fastcgi
 sudo lighttpd-enable-mod fastcgi-php
 sudo service lighttpd force-reload
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password t00r'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password t00r'
-sudo apt-get -y install mysql-server
+echo "mysql-server-5.5 mysql-server/root_password password T00R" | sudo debconf-set-selections
+echo "mysql-server-5.5 mysql-server/root_password_again password T00R" | sudo debconf-set-selections
+sudo apt-get -y install mysql-server-5.5
 sudo mysql_install_db
 sudo /usr/bin/mysql_secure_installation
+
