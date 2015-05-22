03049cbb2985dc882e31cb9a40b170dba7db4f93
Zahari Zahariev <zahari.ven.zahariev@hp.com>
2015-05-22 06:13:51 +0100

    Edit MySQL deploy with password

--- install/deploy.sh
+++ install/deploy.sh
@@ -1,7 +1,12 @@
-sudo aptitude install lighttpd php5-cgi php5-mysql
+sudo apt-get remove lighttpd php5-cgi php5-mysql
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt-get install lighttpd php5-cgi php5-mysql
 sudo lighttpd-enable-mod fastcgi
 sudo lighttpd-enable-mod fastcgi-php
 sudo service lighttpd force-reload
-sudo aptitude install mysql-server
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password t00r'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password t00r'
+sudo apt-get -y install mysql-server
 sudo mysql_install_db
 sudo /usr/bin/mysql_secure_installation
