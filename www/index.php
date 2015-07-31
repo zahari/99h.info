de1f5ea705cdc3a7c32ba03d890971a7fafe5396
Zahari Zahariev <zahari.zahariev@gmail.com>
2015-07-31 12:27:26 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>FREE Hosted Services</title>
     <meta charset="utf-8">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Share Tech Mono">
+
     <style>
         #main_div {
             margin-top: 10%;
             //zoom: 150%;
-            font-family: Courier New;
+            font-family: 'Share Tech Mono';
             font-size: 14pt;
         }
         #bt_div {
@@ -16,14 +20,15 @@
             margin-left: 20px;
         }
         .bt {
-            width: 150px;
-            height: 30px;
-            font-family: Courier New;
-            font-size: 12pt;
+            width: 350px;
+            height: 50px;
+            font-family: 'Share Tech Mono';
+            font-size: 14pt;
+            //font-weight: bold;
         }
         #sub_domain {
             padding: 5px;
-            font-family: Courier New;
+            font-family: 'Share Tech Mono';
             font-size: 14pt;
             font-weight: bold;
         }
@@ -43,14 +48,14 @@
     <center>
         <div id="main_div">
             <form action="index.php" method="POST">
-                <div id="title"> FREE hosting </div>
+                <div id="title"> FREE Hosted Services </div>
                 <div id="sub_domain_div">
                     <span id="txt_before"> http:// </span>
                     <input id="sub_domain" type="text"  size="13" maxlength="13" placeholder="sub-domain-10"/>
                     <span id="txt_after"> .99h.info </span>
                 </div>
                 <div id="bt_div">
-                    <input id="bt_admin" class="bt" type="button" name="bt_admin" value="Manage"/>
+                    <input id="bt_admin" class="bt" type="button" name="bt_admin" value="M a n a g e"/>
                     <!--input id="bt_reg" type="button" name="bt_reg" value="Register"/-->
                 <div>
             </form>
