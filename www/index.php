9e8ad93a289ce08e2a418aa3e88b659ed5930cfc
Zahari Zahariev <zahari.zahariev@gmail.com>
2015-07-31 11:50:49 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -5,21 +5,37 @@
     <style>
         #main_div {
             margin-top: 10%;
-            zoom: 150%;
+            //zoom: 150%;
+            font-family: Courier New;
+            font-size: 14pt;
         }
         #bt_div {
-            margin-top: 20px;
+            margin-top: 40px;
         }
         #bt_reg {
             margin-left: 20px;
         }
+        .bt {
+            width: 150px;
+            height: 30px;
+            font-family: Courier New;
+            font-size: 12pt;
+        }
         #sub_domain {
             padding: 5px;
+            font-family: Courier New;
+            font-size: 14pt;
+            font-weight: bold;
         }
         #title {
-            margin-bottom: 20px;
+            margin-bottom: 40px;
             font-size: 150%;
             font-weight: bold;
+        }
+        #prov_img {
+            width: 300px;
+            height: 15px;
+            margin-top: 30px;
         }
     </style>
 <head>
@@ -30,15 +46,18 @@
                 <div id="title"> FREE hosting </div>
                 <div id="sub_domain_div">
                     <span id="txt_before"> http:// </span>
-                    <input id="sub_domain" type="text"/>
+                    <input id="sub_domain" type="text"  size="13" maxlength="13" placeholder="sub-domain-10"/>
                     <span id="txt_after"> .99h.info </span>
                 </div>
                 <div id="bt_div">
-                    <input id="bt_admin" type="submit" name="bt_admin" value="Manage"/>
-                    <input id="bt_reg" type="button" name="bt_reg" value="Register"/>
+                    <input id="bt_admin" class="bt" type="button" name="bt_admin" value="Manage"/>
+                    <!--input id="bt_reg" type="button" name="bt_reg" value="Register"/-->
                 <div>
             </form>
         </div>
+    </center>
+    <center>
+        <img id="prov_img" src="provision.png" alt="Service provision email">
     </center>
 </body>
 </html>
